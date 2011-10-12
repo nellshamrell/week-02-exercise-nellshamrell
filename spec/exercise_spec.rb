28da0ee3e134f03605524e4dcae5dc4b44472252
Nell Shamrell <nshamrell@bluebox.net>
2011-10-11 20:37:15 -0700

    I contain a failing test.  Hear my evil laugh.

---
+++ spec/exercise_spec.rb
@@ -0,0 +1,14 @@
+describe "Strings" do
+  context "when calling strip" do
+
+    it "should add two strings together into one string" do
+
+        string_1 = "So say"
+        string_2 = "we all!"
+
+	combined_string = string_1 + string_2
+        combined_string.should == "So say we all!"
+    end
+  end
+
+end
