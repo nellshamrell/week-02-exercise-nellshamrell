0ee089470045c03162f8bfaeea0796bc14ce556f
chase coalwell <c.coalwell@gmail.com>
2011-10-16 17:53:53 -0700

    fixed failing test

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -6,7 +6,7 @@ describe "Strings" do
         string_1 = "So say"
         string_2 = "we all!"
 
-	combined_string = string_1 + string_2
+	combined_string = [string_1, string_2].join(' ')
         combined_string.should == "So say we all!"
     end
   end
